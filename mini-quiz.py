204985d808ae2bbc691675c3b4ad139724ab9298
Piotr Benetkiewicz <pbenetkiewicz@gmail.com>
2022-10-28 19:34:33 +0200

    initial commit

---
+++ mini-quiz.py
@@ -0,0 +1,25 @@
+print ("witaj w moim mini quizie!")
+print ("czy 2+2=5?")
+punkty = 0
+odp=input ("odpowiedz!")
+if odp=="tak":
+    print("zle! :c")
+if odp=="nie":
+    punkty = punkty+1
+    print ("dobrze! dostajesz +1 punkt!")
+print ("czy 12+6=18?")
+odp=input("odpowiedz!")
+if odp=="tak":
+    punkty = punkty+1
+    print("brawo! dostajesz +1 punkt!")
+if odp=="nie":
+    print ("spróbój jeszcze raz!")
+print ("czy 10-6=4?")
+odp=input ("odpowiedz!")
+if odp=="tak":
+    punkty = punkty+1
+    print ("YASSSSS!!!!! dostajesz +1 punkt!")
+if odp=="nie":
+    print ("nope :c")
+print ("punkty:",punkty,"z 3 mozliwych do zdobycia")
+print ("dzieki za robienie quizu i do zobaczenia! :D")
